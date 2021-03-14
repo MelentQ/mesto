d2eb4cd6a22fb659405fff50b0a8ebc6dda86622
Danil Melentev <mejleht@yandex.ru>
2021-03-14 11:41:17 +0300

    style: добавить likeActive, transition для попапа

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-let popup = document.querySelector('.popup');
-let popupOpenBtn = document.querySelector('.profile__button_type_edit');
-let popupCloseBtn = document.querySelector('.popup__close-button');
-let popupChangeBtn = document.querySelector('.input__submit-button');
+let popup = document.querySelector('.popup')
+let popupContent = document.querySelector('.popup__content')
+let popupOpenBtn = document.querySelector('.profile__button_type_edit')
+let popupCloseBtn = document.querySelector('.popup__close-button')
+let popupChangeBtn = document.querySelector('.input__submit-button')
 
-let currentProfileName = document.querySelector('.profile__name');
-let currentProfileActivity = document.querySelector('.profile__activity');
-let newProfileName = document.querySelector('.input__text_type_name');
-let newProfileActivity = document.querySelector('.input__text_type_activity');
+let currentProfileName = document.querySelector('.profile__name')
+let currentProfileActivity = document.querySelector('.profile__activity')
+let newProfileName = document.querySelector('.input__text_type_name')
+let newProfileActivity = document.querySelector('.input__text_type_activity')
 
-function UnhidePopup() {
+function OpenPopup() {
   newProfileName.value = currentProfileName.textContent;
   newProfileActivity.value = currentProfileActivity.textContent;
-  popup.classList.add('popup_show')
+  popup.classList.add('popup_opened')
 }
 
-function HidePopup() {
-  popup.classList.remove('popup_show');
+function ClosePopup() {
+  popup.classList.remove('popup_opened')
 }
 
 function SaveChanges() {
@@ -23,9 +24,16 @@ function SaveChanges() {
   currentProfileActivity.textContent = newProfileActivity.value;
 }
 
-popupOpenBtn.addEventListener('click', UnhidePopup);
-popupCloseBtn.addEventListener('click', HidePopup);
+popupOpenBtn.addEventListener('click', OpenPopup)
+popupCloseBtn.addEventListener('click', ClosePopup)
 popupChangeBtn.addEventListener('click', function() {
   SaveChanges()
-  HidePopup()
-});
+  ClosePopup()
+})
+
+//Нажатие на overlay приводит к закрытию попапа.
+popup.addEventListener('click', ClosePopup)
+popupContent.addEventListener('click', function(e) {
+  e.preventDefault()
+  e.stopImmediatePropagation()
+})
